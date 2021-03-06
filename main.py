f343a7fa98aa73c66c2cac98cbbdc2fdaf90c95a
gBlaku <gentblaku@gmail.com>
2021-03-06 01:10:46 +0000

    basic quote bot

--- main.py
+++ main.py
@@ -1,19 +1,40 @@
 import discord
 import os
+import requests
+import json
+
 
 client = discord.Client()
 
+
+
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " -" + json_data[0]['a']
+  return (quote)
+
+
+
+
+
+
+
+
+
+
 @client.event
 async def on_ready():
-  print("I don't like this place....")
+  print("I'm online! My ID is {0.user}".format(client))
 
 @client.event
 async def on_message(message):
   if message.author == client.user:
     return  
 
-  if message.content.startswith('!'):
-    await message.channel.send('You summoned me')
+  if message.content.startswith('!quote'):
+    randomQuote = get_quote()
+    await message.channel.send(randomQuote)
 
   
 client.run(os.getenv('TOKEN'))
